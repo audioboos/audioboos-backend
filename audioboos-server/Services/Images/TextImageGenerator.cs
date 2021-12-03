99caeee96b29cf43c6bf61acb79e5f65bf2c00a4
Fergal Moran <fergal.moran@gmail.com>
2021-12-03 20:41:58 +0000

    Remove ImageMagick (no armhf build :( )

--- audioboos-server/Services/Images/TextImageGenerator.cs
+++ audioboos-server/Services/Images/TextImageGenerator.cs
@@ -1,59 +1,61 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
-using AudioBoos.Server.Services.Utils;
-using ImageMagick;
+using CliWrap;
 
 namespace AudioBoos.Server.Services.Images;
 
 public static class TextImageGenerator {
-    private static async Task<MagickImage> _loadImage(string imageName) {
-        var image = new MagickImage(await ResourceReader.ReadResourceAsByteArray(imageName));
-        return image;
-    }
-
-    private static void _addTextToImage(this MagickImage image, string text, int yOffset, int width, int height,
-        MagickColor color, int padding = 5) {
-        var readSettings = new MagickReadSettings {
-            Font = "Calibri",
-            TextGravity = Gravity.Center,
-            FillColor = color,
-            BackgroundColor = MagickColors.Transparent,
-            Height = height,
-            Width = width - (padding * 2)
-        };
-
-        using var caption = new MagickImage($"caption:{text}", readSettings);
-
-        image.Composite(caption, 0 + (padding / 2), yOffset, CompositeOperator.Atop);
-    }
-
-
     public static async Task<byte[]> CreateArtistAvatarImage(string artistName) {
-        using var image = await _loadImage("default-artist-avatar.png");
-        image._addTextToImage(artistName.Substring(0, 1), 0, 64, 64, MagickColors.Azure);
+        var outputFile = $"{Path.GetTempFileName()}.png";
 
-        await using var ms = new MemoryStream();
-        await image.WriteAsync(ms);
-        return ms.ToArray();
+        var args = $@"-background gray -fill azure \
+            -font Roboto-Regular -gravity Center \
+            -size 64x64 caption:{artistName.TrimStart('T', 'h', 'e').Trim().Substring(0, 1)} \
+            {outputFile}";
+
+        var result = await Cli.Wrap("/usr/bin/convert")
+            .WithArguments(args)
+            .WithWorkingDirectory(Path.GetTempPath())
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteAsync();
+
+        var fileContent = await File.ReadAllBytesAsync(outputFile);
+        File.Delete(outputFile);
+        return fileContent;
     }
 
     public static async Task<byte[]> CreateArtistImage(string artistName) {
-        using var image = await _loadImage("default-artist.png");
-        image._addTextToImage(artistName, 0, 300, 200, MagickColors.Azure);
+        var outputFile = $"{Path.GetTempFileName()}.png";
+        var args = $@"-background gray -fill azure \
+            -font Roboto-Regular -gravity Center \
+            -size 300x200 caption:{artistName} \
+            {outputFile}";
+        var result = await Cli.Wrap("/usr/bin/convert")
+            .WithArguments(args)
+            .WithWorkingDirectory(Path.GetTempPath())
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteAsync();
 
-        await using var ms = new MemoryStream();
-        await image.WriteAsync(ms);
-        return ms.ToArray();
+        var fileContent = await File.ReadAllBytesAsync(outputFile);
+        File.Delete(outputFile);
+        return fileContent;
     }
 
     public static async Task<byte[]> CreateAlbumImage(string artistName, string albumName) {
-        using var image = await _loadImage("default-album.png");
-        image._addTextToImage(artistName, 0, 300, 200, MagickColors.Azure);
-        image._addTextToImage(albumName, 200, 300, 100, MagickColors.DarkGray);
+        var text = $"{artistName}\n{albumName}";
+        var outputFile = $"{Path.GetTempFileName()}.png";
+        var args = $@"-background gray -fill azure \
+            -font Roboto-Regular -gravity Center \
+            -size 300x200 caption:'{text} \
+            {outputFile}";
+        var result = await Cli.Wrap("/usr/bin/convert")
+            .WithArguments(args)
+            .WithWorkingDirectory(Path.GetTempPath())
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteAsync();
 
-        await using var ms = new MemoryStream();
-        await image.WriteAsync(ms);
-        return ms.ToArray();
+        var fileContent = await File.ReadAllBytesAsync(outputFile);
+        File.Delete(outputFile);
+        return fileContent;
     }
 }
