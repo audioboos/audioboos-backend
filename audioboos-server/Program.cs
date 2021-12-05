650a98a98502c209885b51c3185aab28c6de0a9f
Fergal Moran <fergal.moran@gmail.com>
2021-12-05 00:24:19 +0000

    Add serilog and revert docker to debian (imagemagick not working under Alpine)

--- audioboos-server/Program.cs
+++ audioboos-server/Program.cs
@@ -2,16 +2,24 @@ using AudioBoos.Server.Services.Startup.SSL;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace AudioBoos.Server;
 
 public class Program {
     public static void Main(string[] args) {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
         CreateHostBuilder(args).Build().Run();
     }
 
     static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder => {
                 webBuilder
                     .UseStartup<Startup>()
