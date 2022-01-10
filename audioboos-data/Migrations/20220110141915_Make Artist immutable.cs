4d52a93ebcaa041bb68c34d56465a65507bd1d39
Fergal Moran <fergal.moran@gmail.com>
2022-01-10 16:56:25 +0000

    Forgot the migrations

---
+++ audioboos-data/Migrations/20220110141915_Make Artist immutable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AudioBoos.Data.Migrations
+{
+    public partial class MakeArtistimmutable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "immutable",
+                schema: "app",
+                table: "artists",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "immutable",
+                schema: "app",
+                table: "artists");
+        }
+    }
+}
