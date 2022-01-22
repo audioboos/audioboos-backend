5e9abebec3422ea7334edec6f70726fe6cab5b24
Fergal Moran <fergal.moran@gmail.com>
2022-01-22 09:10:52 +0000

    Move docker to buildx

---
+++ scripts/build_docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source ~/.prv/github_token && \
+    echo $GITHUB_TOKEN | docker login ghcr.io -u fergalmoran --password-stdin && \
+    docker buildx build \
+        -t ghcr.io/audioboos/audioboos-backend \
+        --file docker/Dockerfile \
+        --platform linux/arm/v7 --push .
